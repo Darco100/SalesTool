e2eddcb7b498f91f4c61c48671316b762ccc2fb7
Darco100 <sobolew100@wp.pl>
2018-08-08 20:24:27 +0200

    Initial checking of sales tool.

---
+++ SalesTool/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        displayGreeting();
+
+    }
+    
+    public static void displayGreeting() {
+        System.out.println("Hello Sales Happy People");
+    }
+
+}
